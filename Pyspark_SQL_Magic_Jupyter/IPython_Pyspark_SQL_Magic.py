f4804d182f3fea411690235a4df5d8949207ef18
Luca Canali <luca.canali@cern.ch>
2019-09-18 17:22:31 +0200

    updated sqlContext to spark session

--- Pyspark_SQL_Magic_Jupyter/IPython_Pyspark_SQL_Magic.py
+++ Pyspark_SQL_Magic_Jupyter/IPython_Pyspark_SQL_Magic.py
@@ -12,7 +12,7 @@
 # Use: %<magic> for line magic or %%<magic> for cell magic.
 #
 # Author: Luca.Canali@cern.ch
-# September 2016
+# September 2016, updated Sep 2019
 #
 
 from IPython.core.magic import register_line_cell_magic
@@ -26,25 +26,25 @@ detailed_explain = True     # Set to False if you want to see only the physical 
 def sql(line, cell=None):
     "Return a Spark DataFrame for lazy evaluation of the sql. Use: %sql or %%sql"
     val = cell if cell is not None else line 
-    return sqlContext.sql(val)
+    return spark.sql(val)
 
 @register_line_cell_magic
 def sql_show(line, cell=None):
     "Execute sql and show the first max_show_lines lines. Use: %sql_show or %%sql_show"
     val = cell if cell is not None else line 
-    return sqlContext.sql(val).show(max_show_lines) 
+    return spark.sql(val).show(max_show_lines)
 
 @register_line_cell_magic
 def sql_display(line, cell=None):
     """Execute sql and convert results to Pandas DataFrame for pretty display or further processing.
     Use: %sql_display or %%sql_display"""
     val = cell if cell is not None else line 
-    return sqlContext.sql(val).limit(max_show_lines).toPandas() 
+    return spark.sql(val).limit(max_show_lines).toPandas()
 
 @register_line_cell_magic
 def sql_explain(line, cell=None):
     "Display the execution plan of the sql. Use: %sql_explain or %%sql_explain"
     val = cell if cell is not None else line 
-    return sqlContext.sql(val).explain(detailed_explain)
+    return spark.sql(val).explain(detailed_explain)
 
 
